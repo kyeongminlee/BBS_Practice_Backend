9c128da959bbe27f87b091ce42f50437f2c7140c
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-04-02 23:55:31 +0900

    Initial Project

---
+++ src/main/java/kyeongmin/jake/lee/bbspractice/BbsPracticeApplication.java
@@ -0,0 +1,13 @@
+package kyeongmin.jake.lee.bbspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kyeongmin/jake/lee/bbspractice/BbsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kyeongmin.jake.lee.bbspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
